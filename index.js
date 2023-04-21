c043d838ae1e3d6791e7e34e0c66def7562e9766
HaiyingWang22 <horizont072@gmail.com>
2023-04-21 23:19:18 +0200

    oop

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import player from "./player.js";
+import meteorite from "./Meteorite.js";
+
+let fighter;
 const cx = 300; 
 const cy = 450; 
 const Max_width = 600;
@@ -5,47 +9,49 @@ const Max_hight = 950;
             // console.log(1);    
 let Fx; 
 let Mya=[]; 
-// let Mya2=[];
 let lyp;//lat one y position
-let Mvp;//Meteorite velocity parameters
-let Mavp;//Meteorite automatic velocity parameters
+let Mvp;//Meteorite velocity parameters(Control of real-time speed)
+let Mavp;//Meteorite automatic velocity parameters（Control speed reference Increasing with time）
 let index;
 let Meteorites = [];
-
+let Met;
 
 function setup(){
     createCanvas(600,900);
     frameRate(60);
 }
+window.setup = setup;
+
 function basic_condition(){
     Fx = 300; 
-    Mya = [-75,  -350, -850]; 
-    Mavp = 5;  
-    Mvp = Mavp;
+    Mya = [-75,  -350, -850]; //Ma position y array
     index = 0;                                          
     for (let i = 0; i < 100; i++) {
             
         const Mx = Math.floor(Math.random() * 530)+25;
-
+        Mavp = 5;  
+        Mvp = 5;
         let My ;
         if(i<Mya.length){
             My =  Mya[i]; 
 
-        }
+        }  
         else{
             const Rem = i % Mya.length;
             const Int = parseInt(i/Mya.length); 
             My =  Mya[Rem]-Max_hight*Int;  
-        }
-        let Pi = Math.random();
-        // Mya2.push(My);
+        } 
+        let Pi = Math.random(); 
         if (i===99){
             lyp=My;
 
         }
-        const M = {x:Mx, y:My ,p:Pi} ;
+        const M = {x:Mx, y:My ,p:Pi, vp:Mvp, avp:Mavp};
         Meteorites.push(M); 
-    }                            
+    }  
+    fighter = new player(Fx,cy);
+
+
 } 
 
 // main program
@@ -53,136 +59,20 @@ basic_condition();
 
 function draw(){  
     clear();
-
     background(29, 29, 29);  
-    F_move();
-    draw_Meteorites();
-    Fighter(); 
+    creat_Meteorites();
+    fighter.draw();
+    fighter.move();
+         
 } 
+window.draw = draw;
 
+function creat_Meteorites(){
 
-
-// Fighter
-function Fighter(){
-    translate(Fx, cy+300);
-    scale(0.4);
-    noStroke();
-    fill(118, 118, 118);
-    //head
-    triangle(-20, -80, 0, -130, 20, -80);
-    //body
-    fill(112, 112, 112);
-    quad(-20, -80, -20, 50, 20, 50, 20, -80);
-    //
-    fill(118, 118, 118);
-    quad(-10, -80, -10, 50, 10, 50, 10, -80);
-    //左引擎
-    fill(100, 100, 100);
-    quad(-65, 50, -65, 35, -40, 25, -40, 50);
-    //左翅膀
-    fill(118, 118, 118);
-    quad(-20, -50, -130, 35, -100, 53, -20, 20);
-    //左翅膀上面的
-    fill(108, 108, 110);
-    quad(-115, 44, -100, 53, -20, 20, -20, -25);
-    //右引擎
-    fill(100, 100, 100);
-    quad(40, 50, 65, 50, 65, 30, 40, 25);
-    //右翅膀
-    fill(118, 118, 118);
-    quad(20, -50, 130, 35, 100, 53, 20, 20);
-    //右翅膀上面的
-    fill(108, 108, 110);
-    quad(20, -25, 20, 20, 100, 53, 115, 44);
-    //尾巴
-    fill(118, 118, 118);
-    quad(-10, 90, -10, 50, 10, 50, 10, 90);
-    //左尾翼
-    fill(118, 118, 118);
-    quad(-55, 75, -45, 90, -10, 85, -10, 55);
-    //左尾翼上面的
-    fill(108, 108, 110);
-    quad(-50, 82, -45, 90, -10, 85, -10, 65);
-    //右尾翼
-    fill(118, 118, 118);
-    quad(10, 55, 10, 85, 45, 90, 55, 75);
-    //右尾翼上面的
-    fill(108, 108, 110);
-    quad(10, 65, 10, 85, 45, 90, 50, 82);
-}
-function F_move(){
-    // Fighter move
-    if (keyIsDown(65)||keyIsDown(37)&&Fx>50) {
-        Fx-=5;
-        // console.log(Fx);
-    }
-    else if (keyIsDown(68)||keyIsDown(39)&&Fx<550) {
-
-        Fx+=5;
-    }
-}
-
-
-// Meteorite
-function Meteorite(M){
-    if(M.y<950){   
-        push(); 
-        fill(255,255,255);
-        translate(M.x, M.y);
-        scale(2);
-        rotate(10*PI*Math.abs(Math.sin(M.p)));
-        M.p+=0.001;
-        noStroke();
-        fill(115, 112, 104);
-        circle(0, 0, 25);
-        fill(161, 155, 142);
-        circle(0, 0, 20);
-        fill(115, 112, 104); 
-        ellipse(6, 7, 4.6, 4);
-        ellipse(-7, -6, 6.2, 5.4);
-        ellipse(-6, 4, 9, 8);
-        ellipse(5, -5, 10, 11.8);
-        ellipse(8.5, 1, 2.5, 2.5);
-        pop();
-    }
-
-}
-
-// Meteorite move
-function M_move(M){
-    if(M.y<950){
-        M.y+=Mvp;
-        if(Mavp<15){
-            Mavp+=0.0001; 
-            // console.log(Mavp);    
-
-            // Mvp+=0.000001; 
-        }
-        if (keyIsDown(87)||keyIsDown(38)&&Mvp<30){
-            Mvp+=0.01;   
-        }
-        else if (keyIsDown(83)||keyIsDown(40)&&Mvp>4){
-            Mvp-=0.005;   
-        }
-        else{ 
-            if(Mvp>Mavp){
-                Mvp-=0.001;   
-            }
-            else if(Mvp< Mavp){
-                Mvp+=0.001;   
-            }
-        }
-    }
-    else{
-        M.y=M.y+lyp; 
-    }
-}
-
-function draw_Meteorites(){
     Meteorites.forEach((M) =>{
-        Meteorite(M);    
-        M_move(M);
-
-    });
-      
+        Met=new meteorite(M,lyp);
+        Met.draw();
+        Met.move(); 
+    });      
 }
+ 
---
+++ meteorite.js
@@ -0,0 +1,58 @@
+export default class meteorite{
+    constructor(M,lyp){
+        this.M=M;
+        this.lyp=lyp;
+    }
+    draw(){
+        if(this.M.y<950){   
+            push(); 
+            fill(255,255,255);
+            translate(this.M.x, this.M.y);
+            scale(2);
+            rotate(10*PI*Math.abs(Math.sin(this.M.p)));
+            this.M.p+=0.001;
+            noStroke();
+            fill(115, 112, 104);
+            circle(0, 0, 25);
+            fill(161, 155, 142);
+            circle(0, 0, 20);
+            fill(115, 112, 104); 
+            ellipse(6, 7, 4.6, 4);
+            ellipse(-7, -6, 6.2, 5.4);
+            ellipse(-6, 4, 9, 8);
+            ellipse(5, -5, 10, 11.8);
+            ellipse(8.5, 1, 2.5, 2.5);
+            pop();
+        }
+    }
+    move(){
+
+        if(this.M.y<950){
+            this.M.y+=this.M.vp;
+            if(this.M.avp<15){
+                this.M.avp+=0.01;  
+            }
+            if (keyIsDown(87)&&this.M.vp<30||keyIsDown(38)&&this.M.vp<30){
+                this.M.vp+=0.2; 
+            }
+            else if (keyIsDown(83)&&this.M.vp>4||keyIsDown(40)&&this.M.vp>4){
+                this.M.vp-=0.25;   
+            }
+            else{ 
+                if(this.M.vp>this.M.avp){
+                    this.M.vp-=0.1;   
+                }
+                else if(this.M.vp< this.M.avp){
+                    this.M.vp+=0.1;   
+                }
+            }
+            
+
+
+        }
+        else{
+            this.M.y=this.M.y+this.lyp; //Refresh position
+
+        }
+    }
+}
